ffd4cc80383964393858cffa579e4546edc84678
Charles Vin <charles.vin@outlook.fr>
2023-04-08 18:45:53 +0200

    SoftMax

--- src/non_linear.py
+++ src/non_linear.py
@@ -88,3 +88,47 @@ class Sigmoide(Module):
 
     def update_parameters(self, gradient_step=0.001):
         ...
+    
+class SoftMax(Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, X):
+        X_exp = np.exp(X)
+        return X_exp / X_exp.sum(axis=1, keepdims=True)
+
+    def backward_update_gradient(self, input, delta):
+        """Rien à apprendre donc return ajoute rien au gradient
+
+        Parameters
+        ----------
+        input : ndarray (batch, input_size)
+            _description_
+        delta : ndarray (input_size, output_size) 
+            _description_
+        """
+        pass
+
+    def backward_delta(self, input, delta):
+        """_summary_
+
+        Parameters
+        ----------
+        input : ndarray (batch, input_size)
+            _description_
+        delta : ndarray (input_size, output_size) 
+            _description_
+
+
+        Returns
+        -------
+        _type_
+            _description_
+        """
+        # assert input.shape[1] == self.input_size, ValueError()
+        # assert delta.shape == (self.input_size, self.output_size), ValueError()
+        softmax = self.forward(input)
+        return delta * (softmax * (1 - softmax))
+        
+    def update_parameters(self, gradient_step=0.001):
+        ...
