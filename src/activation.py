2bbac5ebeec61c3b97d44ed2e85a3af31abee047
dataymeric <13287345+dataymeric@users.noreply.github.com>
2023-05-02 01:00:16 +0200

    fix: deal with high values

--- src/activation.py
+++ src/activation.py
@@ -1,7 +1,16 @@
 import numpy as np
 from .module import Module
 
-__all__ = ["TanH", "Sigmoid", "Softmax", "LogSoftmax", "ReLU", "Softplus"]
+__all__ = [
+    "TanH",
+    "Sigmoid",
+    "StableSigmoid",
+    "Softmax",
+    "LogSoftmax",
+    "ReLU",
+    "LeakyReLU",
+    "Softplus",
+]
 
 
 class TanH(Module):
@@ -26,7 +35,7 @@ class TanH(Module):
     def backward_delta(self, input, delta):
         return delta * (1 - self(input) ** 2)
 
-    def update_parameters(self, learning_rate=0.001):
+    def update_parameters(self, learning_rate):
         pass  # No parameters to update in TanH
 
 
@@ -53,7 +62,30 @@ class Sigmoid(Module):
         sig_X = self(input)
         return delta * sig_X * (1 - sig_X)
 
-    def update_parameters(self, learning_rate=0.001):
+    def update_parameters(self, learning_rate):
+        pass  # No parameters to update in Sigmoid
+
+
+class StableSigmoid(Module):
+    """Numerically stable Sigmoid activation function."""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def zero_grad(self):
+        pass
+
+    def forward(self, X):
+        return np.where(X >= 0, 1 / (1 + np.exp(-X)), np.exp(X) / (1 + np.exp(X)))
+
+    def backward_update_gradient(self, input, delta):
+        pass  # No gradient to update in Sigmoid
+
+    def backward_delta(self, input, delta):
+        sig_X = self(input)
+        return delta * sig_X * (1 - sig_X)
+
+    def update_parameters(self, learning_rate):
         pass  # No parameters to update in Sigmoid
 
 
@@ -70,8 +102,8 @@ class Softmax(Module):
         pass
 
     def forward(self, X):
-        exp_X = np.exp(X - np.max(X, axis=1, keepdims=True))
-        return exp_X / np.sum(exp_X, axis=1, keepdims=True)
+        exp_X = np.exp(X - np.max(X, axis=-1, keepdims=True))  # log-sum-exp trick
+        return exp_X / np.sum(exp_X, axis=-1, keepdims=True)
 
     def backward_update_gradient(self, input, delta):
         pass  # No gradient to update in Softmax
@@ -80,13 +112,24 @@ class Softmax(Module):
         softmax = self(input)
         return delta * (softmax * (1 - softmax))
 
-    def update_parameters(self, learning_rate=0.001):
+    def update_parameters(self, learning_rate):
         pass  # No parameters to update in Softmax
 
 
 class LogSoftmax(Module):
+    """LogSoftmax activation function.
+
+    .. math::
+            \text{LogSoftmax}(x_{i}) =
+            \log \left( \frac{\exp(x_i)}{\sum_j \exp(x_j)} \right)
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
     def forward(self, X):
-        return X - np.log(np.sum(np.exp(X), axis=1, keepdims=True))
+        X_shifted = X - np.max(X, axis=-1, keepdims=True)
+        return X_shifted - np.log(np.sum(np.exp(X_shifted), axis=-1, keepdims=True))
 
     def zero_grad(self):
         pass
@@ -95,9 +138,10 @@ class LogSoftmax(Module):
         pass  # No gradient to update in LogSoftmax
 
     def backward_delta(self, input, delta):
-        return delta - np.exp(self(input))
+        softmax = np.exp(self(input))
+        return delta - softmax * np.sum(delta, axis=-1, keepdims=True)
 
-    def update_parameters(self, learning_rate=0.001):
+    def update_parameters(self, learning_rate):
         pass  # No parameters to update in LogSoftmax
 
 
@@ -121,10 +165,43 @@ class ReLU(Module):
 
     def backward_delta(self, input, delta):
         """math:: f'(x) = 1 \text{if} x > 0 \text{else} 0."""
-        return delta * (self(input) > 0)
+        return delta * (input > 0)
 
-    def update_parameters(self, learning_rate=0.001):
+    def update_parameters(self, learning_rate):
         pass  # No parameters to update in ReLU
+
+
+class LeakyReLU(Module):
+    """Leaky ReLU activation function.
+    
+    .. math::
+        \text{LeakyReLU}(x) =
+        \begin{cases}
+        x, & \text{ if } x \geq 0 \\
+        \text{negative\_slope} \times x, & \text{ otherwise }
+        \end{cases}
+    """
+
+    def __init__(self, alpha=0.01):
+        super().__init__()
+        self.alpha = alpha
+
+    def zero_grad(self):
+        pass
+
+    def forward(self, X):
+        return np.where(X >= 0, X, self.alpha * X)
+
+    def backward_update_gradient(self, input, delta):
+        pass  # No gradient to update in Leaky ReLU
+
+    def backward_delta(self, input, delta):
+        dx = np.ones_like(input)
+        dx[input <= 0] = self.alpha
+        return delta * dx
+
+    def update_parameters(self, learning_rate):
+        pass  # No parameters to update in Leaky ReLU
 
 
 class Softplus(Module):
@@ -148,5 +225,5 @@ class Softplus(Module):
     def backward_delta(self, input, delta):
         return delta / (1 + np.exp(-input))
 
-    def update_parameters(self, learning_rate=0.001):
+    def update_parameters(self, learning_rate):
         pass  # No parameters to update in Softplus
